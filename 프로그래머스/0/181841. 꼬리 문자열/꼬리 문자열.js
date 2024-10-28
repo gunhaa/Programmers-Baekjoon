c97a40e060a918d3fe2b970d767ebd21657ae14c
gunhaa <168693235+gunhaa@users.noreply.github.com>
2024-10-28 13:52:27 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -0,0 +1,13 @@
+function solution(str_list, ex) {
+    let answer = "";
+    
+    for(let i=0; i<str_list.length ; i++){
+        
+        if(str_list[i].includes(ex) !== true){
+            answer += str_list[i] 
+        }
+        
+    }
+    
+    return answer
+}
