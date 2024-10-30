429d63cb28103c6fff9bd16a1d850d36f1a57ac7
gunhaa <168693235+gunhaa@users.noreply.github.com>
2024-10-30 11:49:47 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 6.22 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,20 @@
+function solution(left, right) {
+
+    let answer = 0;
+    
+    for(let i=left ; i<right+1 ; i++){
+        
+        let count = 0;
+        
+        for(let j=1; j<i+1; j++){
+            
+            if(i%j===0) count++; 
+            
+        }
+        if(count%2 ===0) answer += i;
+        else answer -= i;
+        
+    }
+
+    return answer;
+}
