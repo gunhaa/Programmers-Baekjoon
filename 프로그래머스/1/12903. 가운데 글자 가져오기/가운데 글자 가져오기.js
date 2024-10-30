668529ad0aaa699d6596f5d8ef6c9b30158c8f54
gunhaa <168693235+gunhaa@users.noreply.github.com>
2024-10-30 11:42:29 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,9 +1,5 @@
 function solution(s) {
-
-    if(s.length%2 === 0){
-        return s[s.length/2-1]+s[s.length/2]
-    } else {
-        return s[Math.floor(s.length/2)];
-    }
+    
+    return s.length%2 ===0 ? s[s.length/2-1]+s[s.length/2] : s[Math.floor(s.length/2)];
     
 }
