4dc701f291be96a0a06fc1f6fb60e833161929fd
gunhaa <168693235+gunhaa@users.noreply.github.com>
2024-11-07 16:09:07 +0900

    [level 3] Title: 정수 삼각형, Time: 4.90 ms, Memory: 41.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,17 @@
+function solution(triangle) {
+
+    
+    for(let i=1; i<triangle.length; i++){     
+        for (let j = 0; j < triangle[i].length; j++) {
+            if (j === 0) {
+                triangle[i][j] += triangle[i - 1][0];
+            } else if (j === triangle[i].length - 1) {
+                triangle[i][j] += triangle[i - 1][triangle[i - 1].length - 1];
+            } else {
+                triangle[i][j] += Math.max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+            }
+        }     
+    }
+    
+    return Math.max(...triangle[triangle.length-1]);
+}
