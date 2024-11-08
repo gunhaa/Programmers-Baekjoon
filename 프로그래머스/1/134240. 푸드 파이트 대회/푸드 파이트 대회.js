3e16f78c6800cad96ec171127b8b5fb513972dff
gunhaa <168693235+gunhaa@users.noreply.github.com>
2024-11-08 13:18:11 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.65 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,25 @@
+function solution(food) {
+    
+    
+    const useFood = food.map((v,i)=>{
+        
+       if(v%2 === 0 ) return v;
+        else return v-1;
+        
+    });
+
+    // const len = useFood.reduce((acc,v) => acc+v ,0);
+    
+    const answer = [0];
+    
+    for(let i=useFood.length-1 ; i>0; i--){
+
+        for(let j=0; j<useFood[i]; j++){
+            
+            if(j<useFood[i]/2) answer.unshift(i);
+            else answer.push(i);
+            
+        }
+    }    
+    return answer.join("");
+}
