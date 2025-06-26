13102b428ea6b26485b67509d9cc363123e0f86a
gunhaa <168693235+gunhaa@users.noreply.github.com>
2025-06-26 11:57:14 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2456 ms, Memory: 445948 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,150 +1,60 @@
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
-public class Main {
-
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int iter = Integer.parseInt(br.readLine());
-
-        class DualPriorityQueue {
-            final Map<Integer, Integer> syncMap;
-            final PriorityQueue<Integer> maxPriorityQueue;
-            final PriorityQueue<Integer> minPriorityQueue;
-
-            public DualPriorityQueue() {
-                maxPriorityQueue = new PriorityQueue<>(Collections.reverseOrder());
-                minPriorityQueue = new PriorityQueue<>();
-                syncMap = new HashMap<>();
-            }
-
-            void parseOper(String operation, int argument) {
-                switch (operation) {
-                    case "I": {
-                        addQueue(argument);
-                    }
-                    break;
-                    case "D": {
-                        if(argument == 1) {
-                            deleteMax();
-                        }
-
-                        if(argument == -1) {
-                            deleteMin();
-                        }
-                    }
-                    break;
-                }
-            }
-
-            private void deleteMax() {
-                while(!this.maxPriorityQueue.isEmpty()) {
-                    Integer max = this.maxPriorityQueue.peek();
-                    Integer syncCount = this.syncMap.getOrDefault(max, 0);
-                    if (syncCount == 0) {
-                        this.maxPriorityQueue.poll();
-                        this.syncMap.remove(max);
-                        continue;
-                    }
-                    syncMap.put(max, syncCount - 1);
-
-//                    if (syncCount == 1) {
-//                        syncMap.remove(max);
-//                    } else {
-//                        syncMap.put(max, syncCount - 1);
-//                    }
-                    this.maxPriorityQueue.poll();
-                    break;
-                }
-            }
-
-            private void deleteMin() {
-                while(!this.minPriorityQueue.isEmpty()) {
-                    Integer min = this.minPriorityQueue.peek();
-                    Integer syncCount = this.syncMap.getOrDefault(min, 0);
-                    if (syncCount == 0) {
-                        this.minPriorityQueue.poll();
-                        this.syncMap.remove(min);
-                        continue;
-                    }
-
-                    syncMap.put(min, syncCount - 1);
-//
-//                    if (syncCount == 1) {
-//                        syncMap.remove(min);
-//                    } else {
-//                        syncMap.put(min, syncCount - 1);
-//                    }
-                    this.minPriorityQueue.poll();
-                    break;
-                }
-            }
-
-            private void addQueue(int argument){
-                this.maxPriorityQueue.add(argument);
-                this.minPriorityQueue.add(argument);
-                this.syncMap.put(argument, this.syncMap.getOrDefault(argument, 0) + 1);
-            }
-
-            void display() {
-
-                cleanUp();
-
-//                System.out.println(this.syncMap);
-//                System.out.println(this.maxPriorityQueue);
-//                System.out.println(this.minPriorityQueue);
-
-
-                if(this.minPriorityQueue.isEmpty() && this.maxPriorityQueue.isEmpty()) {
-                    System.out.println("EMPTY");
-                    this.syncMap.clear();
-                    return;
-                }
-                Integer max = this.maxPriorityQueue.peek();
-                Integer min = this.minPriorityQueue.peek();
-                this.minPriorityQueue.clear();
-                this.maxPriorityQueue.clear();
-                this.syncMap.clear();
-                System.out.println(max + " " + min);
-            }
-
-            private void cleanUp() {
-                while (!this.maxPriorityQueue.isEmpty()) {
-                    Integer max = this.maxPriorityQueue.peek();
-                    if (this.syncMap.containsKey(max) && this.syncMap.get(max)>0) {
-                        break;
-                    }
-                    this.syncMap.remove(max);
-                    this.maxPriorityQueue.poll();
-                }
-
-                while (!this.minPriorityQueue.isEmpty()) {
-                    Integer min = this.minPriorityQueue.peek();
-                    if (this.syncMap.containsKey(min)&& this.syncMap.get(min)>0) {
-                        break;
-                    }
-                    this.syncMap.remove(min);
-                    this.minPriorityQueue.poll();
-                }
-            }
-        }
-
-        DualPriorityQueue dpq = new DualPriorityQueue();
-
-        for (int i = 0; i < iter; i++) {
-            int size = Integer.parseInt(br.readLine());
-            for (int j = 0; j < size; j++) {
+        TreeMap<Integer, Integer> tm = new TreeMap<>();
+        int tc = Integer.parseInt(br.readLine());
+        for (int i = 0; i < tc; i++) {
+            int operation = Integer.parseInt(br.readLine());
+            for (int j = 0; j < operation; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                String operation = st.nextToken();
-                int argument = Integer.parseInt(st.nextToken());
-                dpq.parseOper(operation, argument);
+                char operand = st.nextToken().charAt(0);
+                int arg = Integer.parseInt(st.nextToken());
+
+                if(operand == 'I') {
+                    tm.put(arg, tm.getOrDefault(arg, 0) + 1);
+                    continue;
+                }
+
+                if(operand == 'D') {
+                    if(tm.isEmpty()) {
+                        continue;
+                    }
+
+                    if(arg == 1) {
+                        Integer maxKey = tm.lastKey();
+                        Integer remain = tm.get(maxKey);
+                        if(remain == 1) {
+                            tm.remove(maxKey);
+                        } else {
+                            tm.put(maxKey, remain-1);
+                        }
+                    } else {
+                        Integer minKey = tm.firstKey();
+                        Integer remain = tm.get(minKey);
+                        if(remain == 1) {
+                            tm.remove(minKey);
+                        } else {
+                            tm.put(minKey, remain-1);
+                        }
+                    }
+                }
             }
-            // 최대값, 최소값, 혹은 EMPTY
-            dpq.display();
+            if(tm.isEmpty()) {
+                System.out.println("EMPTY");
+            } else {
+                Integer min = tm.firstKey();
+                Integer max = tm.lastKey();
+                System.out.println(max + " " + min);
+                tm.clear();
+            }
         }
     }
 }
